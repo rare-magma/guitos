c3ff0a5a83081c239037b7e7997f03d602c77196
rare-magma <nun0@posteo.co>
2023-05-07 14:39:41 +0200

    feat: cleaner stats labels

--- src/components/StatCard/StatCard.tsx
+++ src/components/StatCard/StatCard.tsx
@@ -104,17 +104,17 @@ function StatCard({
                 d="M1.5 1.5A.5.5 0 0 0 1 2v4.8a2.5 2.5 0 0 0 2.5 2.5h9.793l-3.347 3.346a.5.5 0 0 0 .708.708l4.2-4.2a.5.5 0 0 0 0-.708l-4-4a.5.5 0 0 0-.708.708L13.293 8.3H3.5A1.5 1.5 0 0 1 2 6.8V2a.5.5 0 0 0-.5-.5z"
               />
             </svg>
+            <OverlayTrigger
+              placement="top"
+              overlay={
+                <Tooltip id={`tooltip-available`} style={{ position: "fixed" }}>
+                  available with goal = available - savings estimate
+                </Tooltip>
+              }
+            >
+              <div style={{ marginLeft: ".5rem" }}>with goal</div>
+            </OverlayTrigger>
           </InputGroup.Text>
-          <OverlayTrigger
-            placement="top"
-            overlay={
-              <Tooltip id={`tooltip-available`} style={{ position: "fixed" }}>
-                = available - savings estimate
-              </Tooltip>
-            }
-          >
-            <InputGroup.Text>with goal</InputGroup.Text>
-          </OverlayTrigger>
           <CurrencyInput
             id="with-goal"
             key={stat.withGoal + "-withGoal"}
@@ -178,16 +178,31 @@ function StatCard({
           </InputGroup.Text>
         </InputGroup>
         <InputGroup className="mb-1" key={"saved"}>
-          <OverlayTrigger
-            placement="top"
-            overlay={
-              <Tooltip id={`tooltip-available`} style={{ position: "fixed" }}>
-                = savings goal * available / 100
-              </Tooltip>
-            }
-          >
-            <InputGroup.Text>savings estimate</InputGroup.Text>
-          </OverlayTrigger>
+          <InputGroup.Text>
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="16"
+              height="16"
+              fill="currentColor"
+              className="bi bi-arrow-return-right"
+              viewBox="0 0 16 16"
+            >
+              <path
+                fillRule="evenodd"
+                d="M1.5 1.5A.5.5 0 0 0 1 2v4.8a2.5 2.5 0 0 0 2.5 2.5h9.793l-3.347 3.346a.5.5 0 0 0 .708.708l4.2-4.2a.5.5 0 0 0 0-.708l-4-4a.5.5 0 0 0-.708.708L13.293 8.3H3.5A1.5 1.5 0 0 1 2 6.8V2a.5.5 0 0 0-.5-.5z"
+              />
+            </svg>
+            <OverlayTrigger
+              placement="top"
+              overlay={
+                <Tooltip id={`tooltip-available`} style={{ position: "fixed" }}>
+                  savings estimate = savings goal * available / 100
+                </Tooltip>
+              }
+            >
+              <div style={{ marginLeft: ".5rem" }}>estimate</div>
+            </OverlayTrigger>
+          </InputGroup.Text>
           <CurrencyInput
             id="saved"
             key={stat.saved + "-saved"}
