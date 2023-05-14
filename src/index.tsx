cd0c317015475bf78973ef9714d6e6ae642964f0
rare-magma <nun0@posteo.co>
2023-05-14 20:32:10 +0200

    feat: set up PWA and update checks

--- src/index.tsx
+++ src/index.tsx
@@ -2,6 +2,17 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
+import { registerSW } from "virtual:pwa-register";
+
+const updateSW = registerSW({
+  onNeedRefresh() {
+    // eslint-disable-next-line no-restricted-globals
+    if (confirm("A new version of guitos is available. Update?")) {
+      // eslint-disable-next-line @typescript-eslint/no-floating-promises
+      updateSW(true);
+    }
+  },
+});
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
