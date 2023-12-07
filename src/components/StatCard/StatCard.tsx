74505b0adcc84786d6d7828d9cc4a11bbc5b5284
rare-magma <rare-magma@posteo.eu>
2023-12-07 19:34:54 +0100

    refactor: use icon from lib
    Signed-off-by: rare-magma <rare-magma@posteo.eu>

--- src/components/StatCard/StatCard.tsx
+++ src/components/StatCard/StatCard.tsx
@@ -169,20 +169,7 @@ export function StatCard({
         </InputGroup>
         <InputGroup className="mb-1" key={"withGoal"}>
           <InputGroup.Text>
-            <svg
-              aria-hidden
-              xmlns="http://www.w3.org/2000/svg"
-              width="16"
-              height="16"
-              fill="currentColor"
-              className="bi bi-arrow-return-right"
-              viewBox="0 0 16 16"
-            >
-              <path
-                fillRule="evenodd"
-                d="M1.5 1.5A.5.5 0 0 0 1 2v4.8a2.5 2.5 0 0 0 2.5 2.5h9.793l-3.347 3.346a.5.5 0 0 0 .708.708l4.2-4.2a.5.5 0 0 0 0-.708l-4-4a.5.5 0 0 0-.708.708L13.293 8.3H3.5A1.5 1.5 0 0 1 2 6.8V2a.5.5 0 0 0-.5-.5z"
-              />
-            </svg>
+            <BsArrowReturnRight />
             <OverlayTrigger
               placement="top"
               overlay={
@@ -256,20 +243,7 @@ export function StatCard({
         </InputGroup>
         <InputGroup className="mb-1" key={"saved"}>
           <InputGroup.Text>
-            <svg
-              aria-hidden
-              xmlns="http://www.w3.org/2000/svg"
-              width="16"
-              height="16"
-              fill="currentColor"
-              className="bi bi-arrow-return-right"
-              viewBox="0 0 16 16"
-            >
-              <path
-                fillRule="evenodd"
-                d="M1.5 1.5A.5.5 0 0 0 1 2v4.8a2.5 2.5 0 0 0 2.5 2.5h9.793l-3.347 3.346a.5.5 0 0 0 .708.708l4.2-4.2a.5.5 0 0 0 0-.708l-4-4a.5.5 0 0 0-.708.708L13.293 8.3H3.5A1.5 1.5 0 0 1 2 6.8V2a.5.5 0 0 0-.5-.5z"
-              />
-            </svg>
+            <BsArrowReturnRight />
             <OverlayTrigger
               placement="top"
               overlay={
