bc64a66ed4669079dbf750a703482dcddf00e55c
rare-magma <rare-magma@posteo.eu>
2024-06-23 12:16:50 +0200

    ci: delete unused script
    Signed-off-by: rare-magma <rare-magma@posteo.eu>

--- semantic-release.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-set -Eeo pipefail
-
-git config user.name 'github-actions[bot]'
-git config user.email '41898282+github-actions[bot]@users.noreply.github.com'
-git add CHANGELOG.md package.json
-git commit --message "chore(release): $(cat .version)"
-git push
