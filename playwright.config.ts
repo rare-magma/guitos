ca8bdad18ba0d0a0f4f8a5f800eca57d4ab0b007
rare-magma <rare-magma@posteo.eu>
2025-05-31 14:39:14 +0200

    test: run on production build
    Signed-off-by: rare-magma <rare-magma@posteo.eu>

--- playwright.config.ts
+++ playwright.config.ts
@@ -27,7 +27,7 @@ export default defineConfig({
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     /* Base URL to use in actions like `await page.goto('/')`. */
-    baseURL: "http://localhost:5173",
+    baseURL: "http://localhost:4173",
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: "on-first-retry",
@@ -63,8 +63,8 @@ export default defineConfig({
 
   /* Run your local dev server before starting the tests */
   webServer: {
-    command: "pnpm run start",
-    url: "http://localhost:5173",
+    command: "pnpm run build && pnpm run serve",
+    url: "http://localhost:4173",
     reuseExistingServer: !process.env.CI,
   },
 });
