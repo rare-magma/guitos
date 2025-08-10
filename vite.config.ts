1a8c84334db5d35e84895dd9c46e4df6020fb37c
rare-magma <rare-magma@posteo.eu>
2025-08-10 11:38:53 +0200

    revert: swap vite-plugin-sri3 with vite-plugin-sri-gen
    Signed-off-by: rare-magma <rare-magma@posteo.eu>

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,7 @@
 import react from "@vitejs/plugin-react-oxc";
 import { defineConfig } from "vite";
 import { VitePWA } from "vite-plugin-pwa";
-import sri from "vite-plugin-sri-gen";
+import { sri } from "vite-plugin-sri3";
 
 // biome-ignore lint/style/noDefaultExport: vite config
 export default defineConfig((_) => {
